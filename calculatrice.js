42ad8f82c29ee57bece5b84f8514a561ecf76463
florent <florent.bailly62400@gmail.com>
2019-07-25 15:18:13 +0200

    3eme commit

--- calculatrice.js
+++ calculatrice.js
@@ -1,6 +1,28 @@
-let firstbtn = document.getElementById("zero");
-console.log(firstbtn);
+function affichenombre (){
+    document.getElementById("result").innerHTML += this.value;
+}
 
+function afficheresultat () {
+    document.getElementById("result").innerHTML += (eval('2 + 2'));
+}
+
+let action = document.getElementsByClassName("action"); document.getElementsByClassName("egale");
+for (let i=0; i < action.length; i++)
+{
+    action[i].addEventListener("click", affichenombre);
+}
+
+let op = document.getElementsByClassName("op")
+for (let i=0; i < op.length; i++)
+{
+    op[i].addEventListener("click", affichenombre);
+}
+
+let egale = document.getElementsByClassName("egale")
+for (let i=0; i < egale.length; i++)
+{
+    egale[i].addEventListener("click", affichenombre);
+}
 
 
 
