43b48482c9ed52580df809515d2e97c0a253e55e
florent <florent.bailly62400@gmail.com>
2019-07-25 12:12:34 +0200

    first commit

---
+++ calculatrice.js
@@ -0,0 +1,13 @@
+let action = document.getElementsByClassName("action");
+console.log(action);
+
+action.addEventListener("click", console.log(action));
+
+
+
+
+
+
+
+
+// faire en sorte que quand tu cliques sur un bouton il l'affiche dans console log
